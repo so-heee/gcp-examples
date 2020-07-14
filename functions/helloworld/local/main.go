aec19634d5c2cdd23e9a09c90d7cd1fc48b7eeff
so-heee <so.heee.mochi@gmail.com>
2020-07-14 10:22:28 +0900

    main追加

---
+++ functions/helloworld/local/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	function "example.com/helloworld"
+	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
+)
+
+func main() {
+	funcframework.RegisterEventFunction("/", function.HelloPubSub)
+	port := "8080"
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		port = envPort
+	}
+
+	if err := funcframework.Start(port); err != nil {
+		log.Fatalf("funcframework.Start: %v\n", err)
+	}
+}
