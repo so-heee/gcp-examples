582303b254ef4a620e06cc1db2037319d8f7e05d
so-heee <so.heee.mochi@gmail.com>
2020-07-14 10:19:35 +0900

    first commit

---
+++ functions/helloworld/function.go
@@ -0,0 +1,22 @@
+// Package function provides a set of Cloud Functions samples.
+package function
+
+import (
+	"context"
+	"log"
+)
+
+// PubSubMessage is the payload of a Pub/Sub event.
+type PubSubMessage struct {
+	Data []byte `json:"data"`
+}
+
+// HelloPubSub consumes a Pub/Sub message.
+func HelloPubSub(ctx context.Context, m PubSubMessage) error {
+	name := string(m.Data)
+	if name == "" {
+		name = "World"
+	}
+	log.Printf("Hello, %s!", name)
+	return nil
+}
